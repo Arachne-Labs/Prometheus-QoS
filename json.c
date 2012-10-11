05a9080997b372eb6003e407fc20b8ec80b8c085
xchaos <xchaos@251d49ef-1d17-4917-a970-b30cf55b089b>
2012-10-11 21:30:48 +0000

    json.c
    git-svn-id: https://dev.arachne.cz/repos/prometheus/trunk@202 251d49ef-1d17-4917-a970-b30cf55b089b

---
+++ json.c
@@ -0,0 +1,37 @@
+/* Modified by: xChaos, 20121011 */
+
+#include "cll1-0.6.2.h"
+#include "ipstruct.h"
+
+extern struct IP *ips, *ip;
+
+void write_json_traffic(char *json)
+{
+  FILE *f=fopen(json, "w");
+  if(f > 0)
+  {
+   int jsoncount=0;
+   fprintf(f, "{\n");
+   for_each(ip, ips)
+   {
+    if(      ip->lmsid > 0 
+        and (ip->traffic or ip->direct or ip->proxy or ip->upload))
+    {
+     if(jsoncount)
+     {
+      fprintf(f, ",\n");
+     }
+     fprintf(f, " \"%s\":{ \"lms\": %d, \"ip\":\"%s\", \"total\":%Lu, \"down\":%Lu, \"proxy\":%Lu, \"up\":%Lu, \"min\":%d, \"max\":%d, \"limit\":%d }",
+                ip->name, ip->lmsid, ip->addr, ip->traffic, ip->direct, ip->proxy, ip->upload, ip->min, ip->desired, ip->max);
+     jsoncount++;
+    }
+   }
+   fprintf(f, "}\n");
+   fclose(f);
+   puts("done.");
+  }
+  else
+  {
+   perror(json);
+  }
+}
